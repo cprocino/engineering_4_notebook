a63da440b1e2b4cdeffe049fa325ce91b2fe9d10
cprocino <cprocin27@charlottesvilleschools.org>
2023-10-19 13:45:15 -0400

    asa

---
+++ raspberry-pi/functions1.py
@@ -0,0 +1,24 @@
+def tri(x1, y1, x2, y2, x3, y3):
+        area = (x1*(y2 - y3) + x2*(y3 - y1) + x3*(y1 - y2))/2
+        return area
+
+while True:
+        print (" first cordinates in x,y:")
+        one = input()
+        one = one.split(",")
+        x1 = (int(one[0]))
+        y1 = (int(one[1]))
+        print (" second cordinates in x,y:")
+        two = input()
+        two = two.split(",")
+        x2 = (int(two[0]))
+        y2 = (int(two[1]))
+        print (" third cordinates in x,y:")
+        third = input()
+        third =third.split(",")
+        x3 = (int(third[0]))
+        y3 = (int(third [1]))
+        var = tri(x1, y1, x2, y2, x3, y3)
+        print (f"the area is:" )
+        print (var)
+
