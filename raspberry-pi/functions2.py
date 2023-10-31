7e7313a8405775a52d9861a1fc073bfce003613f
cprocino <cprocin27@charlottesvilleschools.org>
2023-10-31 13:30:46 -0400

    saadsdsads

--- raspberry-pi/functions2.py
+++ raspberry-pi/functions2.py
@@ -1,38 +1,97 @@
+# Import necessary libraries and modules
 import board
 import busio
-import digitalio
-import adafruit_mpu6050
+from adafruit_display_text import label
 from adafruit_display_shapes.triangle import Triangle
-import adafruit_displayio_ssd1306
 from adafruit_display_shapes.line import Line
 from adafruit_display_shapes.circle import Circle
-
+import adafruit_displayio_ssd1306
 import terminalio
 import displayio
 
-
-
+# Define a function 'tri' to calculate the area of a triangle given its coordinates.
 def tri(x1, y1, x2, y2, x3, y3):
-        area = (x1*(y2 - y3) + x2*(y3 - y1) + x3*(y1 - y2))/2
-        return area
+    area = (x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) / 2
+    return area
 
+# Release any existing displays (if any)
+displayio.release_displays()
+
+# Define the SDA (data) and SCL (clock) pins for I2C communication
+sda_pin = board.GP14
+scl_pin = board.GP17
+
+# Create an I2C object for communication
+c = busio.I2C(scl_pin, sda_pin)
+
+# Initialize an I2C display with a specific device address and reset pin
+display_bus = displayio.I2CDisplay(c, device_address=0x3d, reset=board.GP0)
+
+# Create an SSD1306 OLED display object with specific width and height
+display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=128, height=64)
+
+# Initialize an MPU6050 sensor object for gyroscope and accelerometer data
+mpu = adafruit_mpu6050.MPU6050(c, address=0x68)
+
+# Define a digital output pin for signaling (e.g., an LED)
+R = digitalio.DigitalInOut(board.GP1)
+
+# Set the digital output pin to OUTPUT mode
+R.direction = digitalio.Direction.OUTPUT
+
+# Initialize a variable 'initial' with the current time and convert it to an integer
+initial = time.monotonic()
+initial = int(initial)
+
+# Enter an infinite loop
 while True:
-        print (" first cordinates in x,y:")
-        one = input()
-        one = one.split(",")
-        x1 = (int(one[0]))
-        y1 = (int(one[1]))
-        print (" second cordinates in x,y:")
-        two = input()
-        two = two.split(",")
-        x2 = (int(two[0]))
-        y2 = (int(two[1]))
-        print (" third cordinates in x,y:")
-        third = input()
-        third =third.split(",")
-        x3 = (int(third[0]))
-        y3 = (int(third [1]))
-        var = tri(x1, y1, x2, y2, x3, y3)
-        print (f"the area is:" )
-        print (var)
+    # Prompt the user to enter the first set of coordinates in x,y format
+    print("Enter the first coordinates in x,y:")
+    one = input()
+    one = one.split(",")
+    x1 = int(one[0])
+    y1 = int(one[1])
 
+    # Prompt the user to enter the second set of coordinates in x,y format
+    print("Enter the second coordinates in x,y:")
+    two = input()
+    two = two.split(",")
+    x2 = int(two[0])
+    y2 = int(two[1])
+
+    # Prompt the user to enter the third set of coordinates in x,y format
+    print("Enter the third coordinates in x,y:")
+    third = input()
+    third = third.split(",")
+    x3 = int(third[0])
+    y3 = int(third[1])
+
+    # Calculate the area of the triangle using the 'tri' function
+    var = tri(x1, y1, x2, y2, x3, y3)
+
+    # Display the calculated area of the triangle
+    print("The area is:")
+    print(var)
+
+    # Create a displayio Group to manage display content
+    splash = displayio.Group()
+    title = f"{var}km^2"  # Display the area of the triangle at the top
+
+    # Create a label for displaying text on the OLED screen
+    text_area = label.Label(terminalio.FONT, text=title, color=0xFFFF00, x=5, y=5)
+
+    # Create graphical shapes for display
+    hline = Line(0, 32, 128, 32, color=0xFFFF00)  # Horizontal line
+    vline = Line(64, 0, 64, 64, color=0xFFFF00)   # Vertical line
+    circle = Circle(64, 32, 2, outline=0xFFFF00)   # Circle in the center
+    triangle = Triangle(x1 + 64, 32 - y1, x2 + 64, 32 - y2, x3 + 64, 32 - y3, outline=0xFFFF00)
+
+    # Append the graphical elements to the displayio Group
+    splash.append(triangle)
+    splash.append(circle)
+    splash.append(hline)
+    splash.append(vline)
+    splash.append(text_area)
+
+    # Show the displayio Group on the OLED display
+    display.show(splash)
--- raspberry-pi/tester.py
+++ raspberry-pi/tester.py
@@ -1,56 +1,59 @@
+# type: ignore
 import board
 import busio
-import digitalio
-import time
-import adafruit_mpu6050
 from adafruit_display_text import label
+from adafruit_display_shapes.triangle import Triangle
+from adafruit_display_shapes.line import Line
+from adafruit_display_shapes.circle import Circle
 import adafruit_displayio_ssd1306
 import terminalio
 import displayio
+
+def triangle_area(x1, y1, x2, y2, x3, y3):  #defines a function for triangle area that takes six inputs
+    total = (x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) / 2 #the math behind the function, using the six inputs
+    return abs(total)   #returns absolute value of previous math
 
 displayio.release_displays()
 
 sda_pin = board.GP16
 scl_pin = board.GP17
-i2c = busio.I2C(scl_pin, sda_pin)
+i2c = busio.I2C(scl_pin, sda_pin)       #sets up i2c connection
 
-display_bus = displayio.I2CDisplay(i2c, device_address=0x3d, reset=board.GP0)
+display_bus = displayio.I2CDisplay(i2c, device_address=0x3d, reset=board.GP0)   #sets up oled screen with i2c communication
 display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=128, height=64)
 
-mpu = adafruit_mpu6050.MPU6050(i2c, address=0x68)
-R = digitalio.DigitalInOut(board.GP1)
-R.direction = digitalio.Direction.OUTPUT
-
-initial = time.monotonic()
-initial = int(initial)
-
 while True:
-    splash = displayio.Group()
-    title = "ANGULAR VELOCITY"
-    X = round(mpu.gyro[0], 3)
-    Y = round(mpu.gyro[1], 3)
-    Z = round(mpu.gyro[2], 3)
-    x = f"X: {X} rad/s"
-    y = f"Y: {Y} rad/s"
-    z = f"Z: {Z} rad/s"
-    text_area = label.Label(terminalio.FONT, text=title, color=0xFFFF00, x=5, y=5)
-    text_area2 = label.Label(terminalio.FONT, text=x, color=0xFFFF00, x=5, y=20)
-    text_area3 = label.Label(terminalio.FONT, text=y, color=0xFFFF00, x=5, y=35)
-    text_area4 = label.Label(terminalio.FONT, text=z, color=0xFFFF00, x=5, y=50)
-    splash.append(text_area)
-    splash.append(text_area2)
-    splash.append(text_area3)
-    splash.append(text_area4)
-    display.show(splash)
-    if time.monotonic() - initial > 1:
-        print(f"X Acceleration: {mpu.acceleration[0]} m/s^2")
-        print(f"Y Acceleration: {mpu.acceleration[1]} m/s^2")
-        print(f"Z Acceleration: {mpu.acceleration[2]} m/s^2")
-        print("")
-        initial += 1
-    if mpu.acceleration[0] > 9.5 or mpu.acceleration[0] < -9.5:
-        R.value = True
-    elif mpu.acceleration[1] > 9.5 or mpu.acceleration[1] < -9.5:
-        R.value = True
-    else:
-        R.value = False
+    try:        #tries this code, runs normally with no errors
+        print("Enter the first coordinate in format x,y:")
+        first = input()             #takes first input/coordinate
+        first = first.split(",")    #splits input at the comma, separating into x and y
+        x1 = int(first[0])    #calls on first value (x)
+        y1 = int(first [1])   #calls on second value (y)
+        print("Enter the second coordinate in format x,y:")
+        second = input()
+        second = second.split(",")
+        x2 = int(second[0])
+        y2 = int(second [1])
+        print("Enter the third coordinate in format x,y:")
+        third = input()
+        third = third.split(",")
+        x3 = int(third[0])
+        y3 = int(third [1])
+
+        area = triangle_area(x1, y1, x2, y2, x3, y3)    #calls on triangle area function using the recorded inputs
+        print(f"The area of the triangle with vertices ({first[0]},{first[1]}), ({second[0]},{second[1]}), and ({third[0]},{third[1]}) is {area} square km.")
+        splash = displayio.Group()
+        title = f"{area}km^2"       #shows area of triangle at the top
+        text_area = label.Label(terminalio.FONT, text=title, color=0xFFFF00, x=5, y=5)
+        hline = Line(0,32,128,32, color=0xFFFF00)   #horizontal line
+        vline = Line(64,0,64,64, color=0xFFFF00)    #vertical line
+        circle = Circle(64, 32, 2, outline=0xFFFF00)    #circle in the center
+        triangle = Triangle(x1 + 64, 32 - y1, x2 + 64, 32 - y2, x3 + 64, 32 - y3, outline=0xFFFF00) #triangle, based on input coordinates
+        splash.append(triangle)
+        splash.append(circle)
+        splash.append(hline)
+        splash.append(vline)
+        splash.append(text_area)
+        display.show(splash)
+    except:     #if there's an error, skips to this step and restarts the while True loop
+        print("These points are not a valid triangle. Please try again, and make sure you are using the x,y syntax!")
